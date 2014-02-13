744710dccc082fdfd9dc27c85e11fc818bc4ccab
Caleb Duff <me@calebduff.com>
2014-02-13 12:03:16 -0600

    Slight wording change

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = function chargerFactory(description, amount, chargeMethod){
 	} else if chargeMethod.type == 'Stripe'{
 		//pass info on to the Stripe function
 	} else if chargeMethod.type == 'test' {
-		//pass info on to the mock charge function
+		//pass info on to the test charge function
 	} else {
 		return false
 	}
